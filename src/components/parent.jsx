38a609a16ca4ae5b8ff8894d4f08082eba3e2309
Beke Ijobra <ijobra20@gmail.com>
2021-01-15 15:22:09 +0100

    My original calculator

---
+++ src/components/parent.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import ResultComponent from './ResultComponent'
+import KeyPadComponent from './KeyPadComponent'
+
+class Calculator extends Component {
+    constructor() {
+        super();
+        this.state = { 
+            result: ''
+         }
+    }
+    onClick = button =>{
+        if(button === '='){
+            this.calculate()
+        }
+        else if(button === 'C'){
+            this.reset()
+        }
+        else if (button === 'CE'){
+            this.backspace()
+        }
+        else{
+            this.setState({
+                result: this.state.result+button
+            })
+        }
+    }
+    calculate = ()=>{
+        try{
+            this.setState({
+                result: (eval(this.state.result)||"") + ''
+            })
+        }catch(e){
+            this.setState({
+                result: 'error. Abeg just press CE'
+            })
+        }
+    }
+    reset = ()=>{
+        this.setState({
+            result: ''
+        })
+    }
+    backspace = ()=>{
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    }
+
+    render() { 
+        return ( 
+            <div>
+                <div className = 'calculator-body'>
+                    <h1>Simple React Calculator</h1>
+                    <ResultComponent result = {this.state.result}/>
+                    <KeyPadComponent onClick = {this.onClick}/>
+                </div>
+
+            </div>
+         );
+    }
+}
+ 
+export default Calculator;
